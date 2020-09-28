0a95c376b2716fd2fea6ce7c21881ea33e676a91
lane froehlich <elaine.froehlich@gmail.com>
2020-09-28 18:02:49 -0400

    Adding bulldozer files to learningOOP repository

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bulldozer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var dozer = new BullDozer("yellow", "mini");
+            var dozer2 = new BullDozer("red", "large");
+            Console.WriteLine("Bulldoze1 is " + dozer.color);
+            Console.WriteLine("Bulldoze2 is " + dozer2.color);
+        }
+    }
+
+    public class BullDozer
+    {
+        public BullDozer(string _color, string _size)
+        {
+            this.color = _color;
+            this.size = _size;
+        }
+
+        public string color { get; set; }
+        public string size { get; set; }
+    }
+}
