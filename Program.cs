f584db7202166f90e52a492e8c90b3aa185be37c
lane froehlich <elaine.froehlich@gmail.com>
2020-10-08 18:38:01 -0400

    beginning to learn about polymorphism

--- Program.cs
+++ Program.cs
@@ -1,27 +1,82 @@
-﻿using System;
-
-namespace Bulldozer
-{
-    class MainClass
-    {
+﻿    {
         public static void Main(string[] args)
-        {
-            var dozer = new BullDozer("yellow", "mini");
-            var dozer2 = new BullDozer("red", "large");
-            Console.WriteLine("Bulldoze1 is " + dozer.color);
-            Console.WriteLine("Bulldoze2 is " + dozer2.color);
-        }
+{
+
+    var man = new Man();
+    var cart = new Cart();
+    var alien = new Alien();
+    // this was the bulldozer code
+    //var dozer = new BullDozer("yellow", "mini");
+    //var dozer2 = new BullDozer("red", "large");
+    //Console.WriteLine("Bulldoze1 is " + dozer.color);
+    //Console.WriteLine("Bulldoze2 is " + dozer2.color);
+}
     }
 
-    public class BullDozer
+
+    // this was the bulldozer code
+    //public class BullDozer
+    //{
+    //public BullDozer(string _color, string _size)
+    //{
+    //    this.color = _color;
+    //    this.size = _size;
+    //}
+
+        public class GeneralObject
+{
+
+    public GeneralObject()
     {
-        public BullDozer(string _color, string _size)
-        {
-            this.color = _color;
-            this.size = _size;
-        }
 
-        public string color { get; set; }
-        public string size { get; set; }
     }
+    public bool moves { get; set; }
+    public string color { get; set; }
+}
+
+publis class Human : GeneralObject
+{
+    public Human()
+    {
+
+    }
+    public string name { get; set; }
+}
+
+
+public class Man : Human
+{
+    public Man()
+    {
+
+    }
+}
+
+public class Woman : Human
+{
+    public Woman()
+    {
+
+    }
+}
+
+public class Cart : GeneralObject
+{
+    public Cart()
+    {
+
+    }
+    public string manufacturer { get; set; }
+}
+
+public class Alien : GeneralObject
+{
+    public Alien()
+    {
+
+    }
+    public string planet { get; set; }
+}
+
+
 }
